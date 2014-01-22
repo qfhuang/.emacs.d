42c02ec2b273d04fc14a59c54eb3dd63d78521ed
qfhuang <1531322066@qq.com>
2014-01-22 14:12:00 +0800

    add cscope

--- init.el
+++ init.el
@@ -186,4 +186,21 @@
 
 (add-hook 'c-mode-common-hook 'my-cedet-hook)
 
+;;(require 'xcscope)
+(add-hook 'c-mode-common-hook '(lambda() (require 'xcscope)))
+(cscope-setup) ;;打开cscope-minor-mode,才能显示cscope菜单与使用C-c s绑定的快捷键
+(setq cscope-do-not-update-database t)
+(define-key global-map [(control f3)]  'cscope-set-initial-directory)
+(define-key global-map [(control f4)]  'cscope-unset-initial-directory)
+(define-key global-map [(control f5)]  'cscope-find-this-symbol)
+(define-key global-map [(control f6)]  'cscope-find-global-definition)
+(define-key global-map [(control f7)]  'cscope-find-global-definition-no-prompting)
+(define-key global-map [(control f8)]  'cscope-pop-mark)
+(define-key global-map [(control f9)]  'cscope-history-forward-line)
+(define-key global-map [(control f10)] 'cscope-history-forward-file)
+(define-key global-map [(control f11)] 'cscope-history-backward-line)
+(define-key global-map [(control f12)] 'cscope-history-backward-file)
+(define-key global-map [(meta f9)]  'cscope-display-buffer)
+(define-key global-map [(meta f10)] 'cscope-display-buffer-toggle)
+
 
